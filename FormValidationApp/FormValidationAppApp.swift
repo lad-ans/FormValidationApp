0986797b0040d37973154eefec1602ad26f88b79
Ladino Anselmo <ladans.me@gmail.com>
2025-08-19 20:57:57 +0200

    Initial Commit

---
+++ FormValidationApp/FormValidationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationAppApp.swift
+//  FormValidationApp
+//
+//  Created by ladans on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
